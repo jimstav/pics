bdbae1da491777ea07e3548eeb659ccd6b0d2652
Dimitris Stavroulakis <jimstav@gmail.com>
2024-11-30 12:48:52 +0200

    Running the Search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import searchImages from "./api";
 import SearchBar from "./components/SearchBar";
 
 function App() {
   const handleSubmit = (term) => {
-    console.log("Do a search with", term);
+    searchImages(term);
   };
 
   return (
