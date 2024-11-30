0c8e35d0a889e8350208cc3992e9532799f53c9b
Dimitris Stavroulakis <jimstav@gmail.com>
2024-11-30 12:52:02 +0200

    Reminder on Async-Await

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import searchImages from "./api";
 import SearchBar from "./components/SearchBar";
 
 function App() {
-  const handleSubmit = (term) => {
-    searchImages(term);
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+    console.log(result);
   };
 
   return (
